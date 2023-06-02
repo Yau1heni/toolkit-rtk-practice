4ce8a49108ff6b86d92485e16f87a7145112b651
Yau1heni <lodbrok9@gmail.com>
2023-06-02 12:28:47 +0300

    feat: packs slice

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { appReducer } from "app/store/app-slice";
 import { authReducer } from "features/auth/auth-slice";
+import { packsReducer } from "features/packs/packs-slice";
 
 export const store = configureStore({
   reducer: {
     app: appReducer,
     auth: authReducer,
+    packs: packsReducer,
   },
 });
 
---
+++ src/features/packs/packs-slice.ts
@@ -0,0 +1,75 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { createAppAsyncThunk, thunkTryCatch } from "common/utils";
+import {
+  ArgCreatePackType,
+  FetchPacksResponseType,
+  packsApi,
+  PackType,
+} from "features/packs/packs-api";
+
+const fetchPacks = createAppAsyncThunk<{ packsPage: FetchPacksResponseType }, void>(
+  "packs/fetchPacks",
+  async (_, thunkAPI) => {
+    return thunkTryCatch(thunkAPI, async () => {
+      const res = await packsApi.getPacks();
+      return { packsPage: res.data };
+    });
+  }
+);
+
+const createPack = createAppAsyncThunk<void, ArgCreatePackType>(
+  "packs/createPack",
+  async (arg, thunkAPI) => {
+    const { dispatch } = thunkAPI;
+    return thunkTryCatch(thunkAPI, async () => {
+      await packsApi.createPack(arg);
+      dispatch(fetchPacks());
+    });
+  }
+);
+
+const removePack = createAppAsyncThunk<void, string>("packs/removePack", async (id, thunkAPI) => {
+  const { dispatch } = thunkAPI;
+  return thunkTryCatch(thunkAPI, async () => {
+    await packsApi.removePack(id);
+    dispatch(fetchPacks());
+  });
+});
+
+const updatePack = createAppAsyncThunk<void, PackType>(
+  "packs/updatePack",
+  async (arg, thunkAPI) => {
+    const { dispatch } = thunkAPI;
+    return thunkTryCatch(thunkAPI, async () => {
+      await packsApi.updatePack(arg);
+      dispatch(fetchPacks());
+    });
+  }
+);
+
+const slice = createSlice({
+  name: "packs",
+  initialState: {
+    cardPacks: [] as PackType[],
+    page: 1,
+    pageCount: 4,
+    cardPacksTotalCount: 2000,
+    minCardsCount: 0,
+    maxCardsCount: 100,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchPacks.fulfilled, (state, action) => {
+      const packsPage = action.payload.packsPage;
+      state.cardPacks = packsPage.cardPacks;
+      state.page = packsPage.page;
+      state.pageCount = packsPage.pageCount;
+      state.cardPacksTotalCount = packsPage.cardPacksTotalCount;
+      state.minCardsCount = packsPage.minCardsCount;
+      state.maxCardsCount = packsPage.maxCardsCount;
+    });
+  },
+});
+
+export const packsReducer = slice.reducer;
+export const packsThunks = { fetchPacks, createPack, removePack, updatePack };
